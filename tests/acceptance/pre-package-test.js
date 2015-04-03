9b83e484647bb2c99b1d25969d3f4931d0f97976
Chad Hietala <chadhietala@gmail.com>
2015-04-03 14:59:22 -0700

    Browserifing node modules
    This introduces the ability to browserify imports that come from
    node_modules that are not addons.  This is a similiar approch to
    ember-browserify, but is meant to build an individual built file
    for each item that comes from NPM, where as ember-browserify will
    create "browserified.js" bundle.

--- tests/acceptance/pre-package-test.js
+++ tests/acceptance/pre-package-test.js
@@ -33,7 +33,7 @@ describe('pre-package acceptance', function () {
     });
   });
 
-  it.only('should only include files in the dependency graph', function () {
+  it('should only include files in the dependency graph', function () {
     return prePackager(find('.'), {
       entries: ['example-app']
     }).then(function(results) {
@@ -62,6 +62,7 @@ describe('pre-package acceptance', function () {
       entries: ['example-app']
     }).then(function(results) {
       expect(results.files).to.deep.equal([
+        'browserified/moment/moment.js',
         'ember/ember.js',
         'ember-load-initializers/ember-load-initializers.js',
         'ember-moment/helpers/ago.js',
@@ -71,8 +72,7 @@ describe('pre-package acceptance', function () {
         'example-app/app.js',
         'example-app/config/environment.js',
         'example-app/initializers/ember-moment.js',
-        'example-app/router.js',
-        'browserified/moment/moment.js'
+        'example-app/router.js'
       ]);
 
       // Simulated the removal of ember-moment
