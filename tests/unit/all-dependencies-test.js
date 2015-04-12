70ba8adb73649f44ada2d4dae3c13e15fb3d1b05
Chad Hietala <chadhietala@gmail.com>
2015-04-12 12:21:09 -0700

    Simplify resolution
    We actually want to traverse the graph any time a file is changed.
    This is due to the fact that regardless of if the a file's graph
    has changed we still need to recursivly sync them.

--- tests/unit/all-dependencies-test.js
+++ tests/unit/all-dependencies-test.js
@@ -3,8 +3,7 @@
 var AllDependencies = require('../../lib/all-dependencies');
 var expect = require('chai').expect;
 var fs = require('fs-extra');
-var mori = require('mori');
-var depGraph = mori.toClj(fs.readJSONSync('./tests/fixtures/example-app/dep-graph.json'));
+var depGraph = fs.readJSONSync('./tests/fixtures/example-app/dep-graph.json');
 
 describe('all dependencies unit', function() {
 
@@ -15,7 +14,7 @@ describe('all dependencies unit', function() {
   describe('update', function () {
     it('should place a package into dep-graph keyed off of the package name', function() {
       AllDependencies.update('example-app', depGraph);
-      expect(mori.get(AllDependencies._graph, 'example-app')).to.deep.equal(depGraph);
+      expect(AllDependencies._graph['example-app']).to.deep.equal(depGraph);
     });
 
     it('should throw if no package is given', function() {
@@ -30,13 +29,13 @@ describe('all dependencies unit', function() {
   describe('for', function() {
     it('should return a graph for a package', function() {
       AllDependencies.update('example-app', depGraph);
-      expect(AllDependencies.for('example-app')).to.deep.equal(mori.toClj(depGraph));
+      expect(AllDependencies.for('example-app')).to.deep.equal(depGraph);
     });
 
     it('should return the imports for a specific file', function() {
       AllDependencies.update('example-app', depGraph);
       var imports = AllDependencies.for('example-app/initializers/ember-moment.js');
-      expect(mori.toJs(imports)).to.deep.equal([
+      expect(imports).to.deep.equal([
         'ember-moment/helpers/moment',
         'ember-moment/helpers/ago',
         'ember-moment/helpers/duration',
@@ -45,11 +44,11 @@ describe('all dependencies unit', function() {
     });
 
     it('should return an empty Map if the package graph is not found', function() {
-      expect(AllDependencies.for('example-moment')).to.deep.equal(mori.hashMap());
+      expect(AllDependencies.for('example-moment')).to.deep.equal({});
     });
 
     it('should return an empty List if the file imports are not found', function() {
-      expect(AllDependencies.for('example-moment/ago.js')).to.deep.equal(mori.list());
+      expect(AllDependencies.for('example-moment/ago.js')).to.deep.equal([]);
     });
   });
 });
